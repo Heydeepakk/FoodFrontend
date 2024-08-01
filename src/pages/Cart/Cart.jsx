0fba9333aebc8012d69530a3aaeed46b8e658f3a
deepakevra <deepak@evramedia.com>
2024-08-01 13:10:53 +0530

    as

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -25,7 +25,7 @@ const Cart = () => {
           if(cartItems[item._id]>0){
             return <div>
               <div className="cart-items-title cart-items-item">
-              <img src={url+item.image} alt="" />
+              <img src={item.image} alt="" />
               <p>{item.name}</p>
               <p>₹{item.price}</p>
               <p>{cartItems[item._id]}</p>
